d619b4842d35ac48ee4fe0431d1b99e409a19662
DeepwaterCreations <dylan.craine@deepwatercreations.com>
2015-02-19 22:15:16 -0700

    Fixed the movement speed to properly use delta t.

--- character.py
+++ character.py
@@ -7,7 +7,7 @@ class Character:
     def __init__(self):
         self.x = 0
         self.y = 0
-        self.speed = 10        
+        self.speed = 100        
         
         self.sprite = ""
         
@@ -18,15 +18,18 @@ class Character:
     def loadSprite(self, filepath):
         self.sprite = pygame.image.load(filepath).convert()
         
-    def update(self):
+    def update(self, dt):
+        if dt == 0:
+            return
+        
         if self.key_inputs['up']:
-            self.y -= self.speed
+            self.y -= self.speed * (dt/1000.0)
             
         if self.key_inputs['down']:
-            self.y += self.speed
+            self.y += self.speed * (dt/1000.0) 
             
         if self.key_inputs['left']:
-            self.x -= self.speed
+            self.x -= self.speed * (dt/1000.0)
             
         if self.key_inputs['right']:
-            self.x += self.speed
+            self.x += self.speed * (dt/1000.0)
